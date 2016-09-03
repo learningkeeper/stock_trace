4a879952327a72e7164da86b6b64e9a65411503f
learningkeeper <learning.keeper@outlook.com>
2016-09-03 05:54:45 +0000

    daily update

---
+++ dailydata.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#coding=utf-8
+import sh
+
+
+title = [ u"交易日期", u"成交股數", u"成交金額", u"開盤價", u"最高價", u"最低價", u"收盤價", u"漲跌價差" ,u"成交筆數"]
+
+stock = [ (u"第一金", "data/0050.csv"), ( u"0056", "data/0056.csv"), (u"中華電信", "data/2412.csv")]
+
+def check_stock(x):
+    result = sh.tail('-1', x).split(',')
+    return zip(title, result)
+
+
+def main():
+    with open('text.txt', 'w') as fd:
+		for name, file in stock:
+ 		    fd.write(name.encode('utf-8') + "\n")
+		    for  head, data in check_stock(file):
+ 			    fd.write(head.encode('utf-8') + " : " + data.encode('utf-8') + "\n")
+
+
+if __name__ == "__main__":
+	main()
